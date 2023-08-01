50db0c540a2b8de7880b87beb7d967944168574f
Abigail Acheampomaa Opoku <acheapomaa.abigail.opoku@gmail.com>
2023-08-01 18:43:30 +0000

    first task

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-function getListStudents() {
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
